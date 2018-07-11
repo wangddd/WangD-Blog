3dd71d4c14a325c0f33637dc238f7d5bdc710a6f
Wang Dong <wangdong.2885@gmail.com>
2018-07-12 01:12:47 +0800

    first

---
+++ my_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/12 0:00
+# @Site    : 
+# @File    : my_test.py
+# @Software: PyCharm
+__author__ = 'Wang Dong'
+
+
+import logging
+
+sql = '123'
+
+log(sql)
