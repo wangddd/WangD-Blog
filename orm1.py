e97eef72e944c3a5a9bfb653ae08abc760041d93
Wang Dong <wangdong.2885@gmail.com>
2018-07-12 12:18:07 +0800

    2

---
+++ orm1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/12 2:32
+# @Site    : 
+# @File    : orm1.py
+# @Software: PyCharm
+__author__ = 'Wang Dong'
---
+++ use_db.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/12 1:26
+# @Site    : 
+# @File    : use_db.py
+# @Software: PyCharm
+__author__ = 'Wang Dong'
